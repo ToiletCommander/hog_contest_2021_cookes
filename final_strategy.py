84b7059e708a6531447998567dff62a47f80f580
Windy Feng <1745500559@qq.com>
2021-02-05 13:52:48 +0800

    Add Dice Possibility Test

--- final_strategy.py
+++ final_strategy.py
@@ -9,49 +9,58 @@ import dice as diceLib
 
 PLAYER_NAME = 'QuantumCookie01'  # Change this line!
 
-def playOnce(turnNum, dice, currentScore, opponentScore, numToRoll, lastTimeTrot, targetScore):
-    """simulate one time playing
-    returns (newScore, newOpponentScore, canPlayMoreTurn, isMoreBoar, isTimeTrot)
-    """
-
-    scoreIncrease = gamecalc.take_turn(numToRoll,opponentScore,dice,targetScore)
-    isMoreBoar = gamecalc.more_boar(currentScore+scoreIncrease,opponentScore)
-    isTimeTrot = gamecalc.time_trot(turnNum,numToRoll,lastTimeTrot) if not(isMoreBoar) else False
-    return (currentScore+scoreIncrease,opponentScore,isMoreBoar or isTimeTrot, isMoreBoar, isTimeTrot)
-
 def getWinningChance(currentPlayerLastTimeTrot, opponentLastTimeTrot, numToRoll, selfScore, opponentScore, targetScore, turnNum = 0, currentPlayer = 0):
+    def getWinningChanceForSpecificScoreIncrease(moreTurn, usedTimeTrot, opponentLastTimeTrot, newScore, oldScore, opponentScore, targetScore, turnNum = 0):
+        if(newScore >= targetScore):
+            return 1.0
+
+        sumChance = 0.0
+        numChance = 10
+        if moreTurn:
+            for i in range(0,numChance+1):
+                sumChance += getWinningChance(usedTimeTrot,opponentLastTimeTrot,i,newScore,opponentScore,targetScore,turnNum+1,0)
+        else:
+            for i in range(0,numChance+1):
+                sumChance += 1.0 - getWinningChance(opponentLastTimeTrot,usedTimeTrot,i,opponentScore,newScore,targetScore,0,0)
+        chance = sumChance / numChance
+        getWinningChance.result_dict[currentKey] = chance
+        return chance
+    
+    def winningChanceForDicePossibility(currentLastTimeTrot, opponentLastTimeTrot, currentScore, opponentScore, numDice, targetScore, turnNum = 0, previousNums = []):
+        diceSideNum = 6 if turnNum == 0 else 8
+        if(numDice >= 1):
+            currentNums = previousNums
+            lastIndex = len(currentNums)
+            chanceSum = 0.0
+            chanceTotal = diceSideNum
+            for i in range(1,diceSideNum+1):
+                if(len(currentNums) == lastIndex):
+                    currentNums.append(i)
+                else:
+                    currentNums[lastIndex] = i
+                
+                chanceSum += winningChanceForDicePossibility(currentLastTimeTrot, opponentLastTimeTrot,currentScore,opponentScore,numDice-1,targetScore,turnNum,currentNums)      
+            
+            return chanceSum / chanceTotal
+        else:
+            scoreIncrease = gamecalc.calc_turn_score(previousNums,opponentScore)
+            newScore = currentScore + scoreIncrease
+            moreBoar = gamecalc.more_boar(newScore,opponentScore)
+            timeTrot = gamecalc.time_trot(turnNum,len(previousNums),currentLastTimeTrot)
+            moreTurn = moreBoar or timeTrot
+            return getWinningChanceForSpecificScoreIncrease(moreTurn,timeTrot, opponentLastTimeTrot,newScore,currentScore,opponentScore,targetScore,turnNum)
+
+
+    
     currentKey = (currentPlayerLastTimeTrot,opponentLastTimeTrot,numToRoll,selfScore,opponentScore,targetScore,turnNum > 0,currentPlayer)
     if currentKey in getWinningChance.result_dict:
         return getWinningChance.result_dict[currentKey]
-    if currentPlayer != 0:
+    if not(currentPlayer == 0):
         return 1.0 - getWinningChance(opponentLastTimeTrot,currentPlayerLastTimeTrot,numToRoll,opponentScore,selfScore,targetScore,turnNum,0)
     
     dice = getWinningChance.dice_six if turnNum == 0 else getWinningChance.dice_eight
-
     
-
-    playReturn = playOnce(turnNum,dice,selfScore,opponentScore,numToRoll,currentPlayerLastTimeTrot,targetScore)
-
-    newScore, newOpponentScore, canPlayMoreTurn, usedBoar, usedTimeTrot = playReturn
-    if(newScore >= targetScore):
-        return 1.0
-    if canPlayMoreTurn:
-        sumChance = 0.0
-        numChance = 10
-        for i in range(0,numChance+1):
-            sumChance += getWinningChance(usedTimeTrot,opponentLastTimeTrot,i,newScore,newOpponentScore,targetScore,turnNum+1,0)
-        chance = sumChance / numChance
-        getWinningChance.result_dict[currentKey] = chance
-        return chance
-    else:
-        sumChance = 0.0
-        numChance = 10
-        for i in range(0,numChance+1):
-            sumChance += getWinningChance(usedTimeTrot,opponentLastTimeTrot,i,newScore,newOpponentScore,targetScore,turnNum+1,1)
-        chance = sumChance / numChance
-        getWinningChance.result_dict[currentKey] = chance
-        return chance
-    
+    return winningChanceForDicePossibility(currentPlayerLastTimeTrot,opponentLastTimeTrot,selfScore,opponentScore,numToRoll,targetScore,turnNum)
 
 getWinningChance.dice_eight = diceLib.make_fair_dice(8)
 getWinningChance.dice_six = diceLib.make_fair_dice(6)
